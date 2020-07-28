e132b6f315c14b844c3103c34b7d94db9e1c5df6
Vsevolod425 <glasskitee@gmail.com>
2020-07-28 22:44:05 +0300

    Полчение данных с сервера 1.0

---
+++ js/load.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function () {
+  var URL = 'https://javascript.pages.academy/kekstagram/data.';
+
+  window.load = function (onSuccess, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+    xhr.open('GET', URL);
+    xhr.addEventListener('load', function () {
+      console.log(xhr.response);
+    });
+    xhr.send();
+  };
+})();
